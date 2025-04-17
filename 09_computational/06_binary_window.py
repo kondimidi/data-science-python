4f1920003b41613995ca422e21b034a296c0485d
Konrad <kondektor@gmail.com>
2025-04-17 12:17:21 +0200

    Add files via upload

---
+++ 09_computational/06_binary_window.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: krakowiakpawel9@gmail.com
+@site: e-smartdata.org
+"""
+
+import pandas as pd
+import seaborn as sns
+sns.set()
+
+df = pd.read_csv('./data/ten_d.csv', index_col=0)
+df.columns = ['Open', 'High', 'Low', 'Close', 'Volume']
+
+# %%
+open_price = df['Open']
+close_price = df['Close']
+df['Daily_Change'] = df['Close'].pct_change()
+corr = df.corr()
+
+# %%
+rolling_corr = open_price.rolling(window=60).corr(close_price)
+rolling_corr.plot()
+
+# %%
+df_roll = df.rolling(window=20).corr(df['Close']).drop('Close', axis=1)
+df_roll.plot()
---
+++ 09_computational/07_aggregation.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: krakowiakpawel9@gmail.com
+@site: e-smartdata.org
+"""
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+sns.set()
+
+
+df = pd.read_csv('./data/ten_d.csv', index_col=0)
+df.columns = ['Open', 'High', 'Low', 'Close', 'Volume']
+
+# %%
+df_agg = df.rolling(window=10).agg(np.mean)
+
+# %%
+close_stats = df['Close'].rolling(window=10).agg([np.median, np.mean, np.std])
+
+# %%
+df_stats = df.drop('Volume', axis=1)
+df_stats = df_stats.rolling(window=10).agg([np.mean, np.std])
+
+# %% different aggregation
+
+diff_agg = df.rolling(window=20).agg({'Close': [np.std, np.mean],
+                                      'Volume': np.std})
+
+# %%
+df = (df - df.mean()) / df.std()
+df = df.rolling(window=30).agg({'Close': np.std, 'Volume': np.std})
+df.plot()
+
+# %%
+df.rolling(window=len(df), min_periods=1).mean()[:5]
