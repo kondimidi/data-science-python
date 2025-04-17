9aa4d2cb476bc3a613a270d42f9470728161cf74
Konrad <kondektor@gmail.com>
2025-04-17 12:14:07 +0200

    Add files via upload

---
+++ 03_missing_data/03_drop_na.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: krakowiakpawel9@gmail.com
+@site: e-smartdata.org
+"""
+
+import numpy as np
+import pandas as pd
+
+
+# %% creating dataset
+df = pd.DataFrame(np.random.randn(10, 4),
+                  columns=['one', 'two', 'three', 'four'])
+
+# %%
+for row in df.values:
+    switch = np.random.choice([0, 1])
+    if switch:
+        i = np.random.choice([0, 1, 2, 3])
+        row[i] = np.nan
+
+# %%
+df.describe()
+
+# %% by row
+df.dropna()
+
+
+# %% by col
+df.dropna(axis=1)
