cf0d028df803fc94a1b79c3cfe96906e33ddffbd
Konrad <kondektor@gmail.com>
2025-04-17 12:17:06 +0200

    Add files via upload

---
+++ 08_visualization/01_basic.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: krakowiakpawel9@gmail.com
+@site: e-smartdata.org
+"""
+
+import numpy as np
+import pandas as pd
+
+
+ts = pd.Series(np.random.randn(1000),
+               index=pd.date_range('2019-01-01', periods=1000))
+
+# %%
+ts = ts.cumsum()
+ts.plot()
+
+# %%
+ts = ts.cummin()
+ts.plot()
+
+# %%
+ts = ts.cummax()
+ts.plot()
+
+# %%
+df = pd.DataFrame(np.random.randn(1000, 5),
+                  index=pd.date_range('2019-01-01', periods=1000),
+                  columns=list('ABCDE'))
+
+# %%
+df = df.cumsum()
+df.plot()
+
+df['B'].plot()
+# %%
+df = df.cummin()
+df.plot()
+
+# %%
+df.iloc[5].plot(kind='bar')
---
+++ 08_visualization/03_histogram.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: krakowiakpawel9@gmail.com
+@site: e-smartdata.org
+"""
+
+import numpy as np
+import pandas as pd
+
+
+# %%
+df = pd.DataFrame(np.random.rand(1000))
+
+# %%
+df.hist()
+
+# %%
+df.plot.hist()
+
+# %%
+df.plot(kind='hist')
+
+# %%
+df.plot.hist(bins=20, cmap='viridis')
+
+# %%
+df = pd.DataFrame(np.random.randn(10000))
+df.plot.hist(bins=40, color='red', alpha=0.5)
+
+# %%
+df = pd.DataFrame({'mu_0_sigma_1': np.random.randn(10000),
+                   'mu_1_sigma_2': 2 * np.random.randn(10000) + 1,
+                   'mu_8_sigma_3': 3 * np.random.randn(10000) + 8})
+df.plot.hist(bins=40, cmap='viridis', alpha=0.5,
+             title='Different Normal Distribution')
+
+# %% cumulative plot
+df['mu_8_sigma_3'].plot.hist(cumulative=True, bins=1000, color='green',
+                             alpha=0.5)
+
+# %%
+df.hist(sharex=True, sharey=True)
