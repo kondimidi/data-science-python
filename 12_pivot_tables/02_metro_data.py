f320ef5492008ab792a9477e9d74377e1515c438
Konrad <kondektor@gmail.com>
2025-04-17 12:17:47 +0200

    Add files via upload

---
+++ 12_pivot_tables/02_metro_data.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: krakowiakpawel9@gmail.com
+@site: e-smartdata.org
+"""
+
+import pandas as pd
+import seaborn as sns
+sns.set()
+
+
+url = ('https://archive.ics.uci.edu/ml/machine-learning-databases/00492/'
+       'Metro_Interstate_Traffic_Volume.csv.gz')
+
+# %%
+metro = pd.read_csv(url, compression='gzip', parse_dates=True,
+                    index_col='date_time')
+
+metro = metro.loc['2016-01-01':]
+
+# %%
+traffic = metro.iloc[:, -1:]
+traffic.plot()
+tr = traffic.resample('M').sum()
+tr.plot()
+
+# %%
+metro.pivot_table(values='traffic_volume', index='weather_main').\
+      plot(kind='bar')
+
+metro.groupby('holiday').mean()['traffic_volume'].plot(kind='bar')
