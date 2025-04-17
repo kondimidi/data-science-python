9ebaa38e56bc1ca2e52e4e2de5b83941e1d38ff1
Konrad <kondektor@gmail.com>
2025-04-17 12:17:35 +0200

    Add files via upload

---
+++ 10_options/01_basic.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: krakowiakpawel9@gmail.com
+@site: e-smartdata.org
+"""
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+sns.set()
+
+
+# setting max rows to display
+pd.options.display.max_rows = 10
+
+# %%
+df = pd.DataFrame(np.random.randn(100, 3))
+
+# %% get value of option
+pd.get_option('display.max_rows')
+
+# %% set value of option
+pd.set_option('display.max_rows', 30)
+pd.get_option('display.max_rows')
+
+pd.reset_option('display.max_rows')
+pd.get_option('display.max_rows')
+
+pd.describe_option('display.max_rows')
+pd.describe_option('mode.sim_interactive')
---
+++ 10_options/02_setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: krakowiakpawel9@gmail.com
+@site: e-smartdata.org
+"""
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+sns.set()
+
+# %%
+pd.set_option('display.max_rows', 999)
+pd.set_option('precision', 3)
+
+pd.describe_option('precision')
+pd.get_option('expand_frame_repr')
+pd.set_option('large_repr', 'info')
+# %%
+df = pd.DataFrame(np.random.rand(100, 4),
+                  columns=['a', 'b', 'cust_tab_dev_prod_dict_flg', 'd'])
+print(df)
+# %%
+df.info()
+df.memory_usage()
